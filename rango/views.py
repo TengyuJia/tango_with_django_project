a433b224bc7ab30640a3506723266189e8e1a249
Tengyu Jia <twineone@outlook.com>
2025-03-03 03:19:16 +0800

    add cookie test code

--- rango/views.py
+++ rango/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+ 
 def index(request):
     #return HttpResponse("Rango says hey there partner!<a href=\"http://127.0.0.1:8000/rango/about\">About</a>")
     category_list = Category.objects.order_by('-likes')[:5]
@@ -14,7 +16,7 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-
+    request.session.set_test_cookie()
     return render(request, 'rango/index.html', context=context_dict)
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -30,6 +32,10 @@ def show_category(request, category_name_slug):
 
 def about(request):
     #return HttpResponse("Rango says this is about the page!<a href = \"http://127.0.0.1:8000/rango\">Index</a>")
+
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     return render(request, 'rango/about.html')
     
 def add_category(request):
@@ -123,4 +129,15 @@ def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
 
+ def visitor_cookie_handler(request, response):
+     visits = int(request.COOKIES.get('visits', '1'))
+     last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+     last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+     '%Y-%m-%d %H:%M:%S')
+     if (datetime.now()- last_visit_time).days > 0:
+         visits = visits + 1
+         response.set_cookie('last_visit', str(datetime.now()))
+     else:
+         response.set_cookie('last_visit', last_visit_cookie)
+     response.set_cookie('visits', visits)
             
