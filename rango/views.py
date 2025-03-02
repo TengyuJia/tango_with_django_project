58d482430e61d51b90eebfbcbfc5724ffe067e33
Tengyu Jia <twineone@outlook.com>
2025-03-03 03:33:55 +0800

    client side cookies access

--- rango/views.py
+++ rango/views.py
@@ -16,8 +16,10 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    request.session.set_test_cookie()
-    return render(request, 'rango/index.html', context=context_dict)
+    context_dict['visits'] = int(request.COOKIES.get('visits', '1'))
+    response = render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request, response)
+    return response
 def show_category(request, category_name_slug):
     context_dict = {}
     try:
@@ -129,15 +131,14 @@ def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
 
- def visitor_cookie_handler(request, response):
-     visits = int(request.COOKIES.get('visits', '1'))
-     last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
-     last_visit_time = datetime.strptime(last_visit_cookie[:-7],
-     '%Y-%m-%d %H:%M:%S')
-     if (datetime.now()- last_visit_time).days > 0:
-         visits = visits + 1
-         response.set_cookie('last_visit', str(datetime.now()))
-     else:
-         response.set_cookie('last_visit', last_visit_cookie)
-     response.set_cookie('visits', visits)
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    if (datetime.now()- last_visit_time).days > 0:
+        visits = visits + 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+    response.set_cookie('visits', visits)
             
